78ac4682f0a59f2137da8808b1410350a0188f41
olegaposh <oleg.poshiv@gmail.com>
2021-03-04 21:51:26 -0700

    added bootstrap and some styling

--- src/Pages/search.js
+++ src/Pages/search.js
@@ -4,38 +4,41 @@ import React, { useState, useEffect } from 'react';
 
 function Search() {
 
-const [data, setData] = useState([]);
+    const [data, setData] = useState([]);
 
-useEffect( ()=> {
+    useEffect(() => {
 
-getData();
+        getData();
 
-}, [])
+    }, [])
 
-const getData = async () => {
+    const getData = async () => {
 
-    const url = 'https://api.giphy.com/v1/gifs/trending?api_key=XCj0hwZJ7PyY5ptZE1rKV8hgWXVsKWNa';
-    const result = await fetch(url);
-    const getResult = await result.json();
-    console.log(getResult.data)  
-    setData(getResult.data);
-    
-}
+        const url = 'https://api.giphy.com/v1/gifs/trending?api_key=XCj0hwZJ7PyY5ptZE1rKV8hgWXVsKWNa';
+        const result = await fetch(url);
+        const getResult = await result.json();
+        console.log(getResult.data)
+        setData(getResult.data);
 
-
+    }
 
 
-return(
-    <>
-    <p>TRENDING: </p>
-    {data.map(record => (
 
-        <div key={record.id} className="trending-gifs">
-            <img src={record.images.fixed_height.url}/>
-        </div>
-    ))}
-    </>
-) 
+
+    return (
+        <>
+            <p className="text-center">TRENDING: </p>
+
+            <div className="text-center">
+                {data.map(record => (
+
+                    <div key={record.id} className="trending-gifs">
+                        <img src={record.images.fixed_height.url} />
+                    </div>
+                ))}
+            </div>
+        </>
+    )
 
 }
 
